56e59e14fb02e6bd08fd9eea2364e6f2c0d3b4bc
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2018-09-07 03:20:28 -0300

    cournot

--- R/cournot_2.R
+++ R/cournot_2.R
@@ -14,21 +14,27 @@
  cournot_2 = function(firm1, firm2, demand){
    require(Deriv)
    require(rSymPy)
-   
+
    mg_cost1 = Deriv(firm1)
    mg_cost2 = Deriv(firm2)
-   
+
    profit1 = multiply(mg_cost1,demand) - firm1
    profit2 = multiply(mg_cost2,demand) - firm2
- 
+
    mg_profit1 = Deriv(profit1, x = q1)
    mg_profit2 = Deriv(profit2, x = q2)
-   
+
   q1 = uniroot(mg_profit1, lower = - 10000, upper = 10000)
   q2 = uniroot(mg_profit2, lower = - 10000, upper = 10000)
    q = q1 + q2
-   
-  p = demand(q)
-  } 
-  
-  
+
+   q1sh = q1 / q
+   q2sh = q2 / q
+
+   p = demand(q)
+
+
+  results = list(p = price, q = "total output", q1sh = q1sh, q1sh = q2sh)
+  }
+
+

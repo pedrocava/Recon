db57be9d754ea500ae2ad60b62f586ebaffa704d
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2018-09-11 14:56:48 -0300

    Update cost2.R

--- R/cost2.R
+++ R/cost2.R
@@ -1,14 +1,14 @@
 #' Cost function
 #'
 #' This function provides a general cost curve in a symbolic of numeric manner. Needed to use some functions of this package.
-#' @param q1 is output.
-#' @param q0_1 is the firm's fixed cost. Defatults to 0.
-#' @param c1 is linear marginal cost. Defatults to 1.
-#' @param c1_2 is the quadratic term's coefficient. Defaults to zero.
+#' @param q2 is output.
+#' @param q0_2 is the firm's fixed cost. Defatults to 0.
+#' @param c2 is linear marginal cost. Defatults to 1.
+#' @param c2_2 is the quadratic term's coefficient. Defaults to zero.
 #' @keywords microeconomics imperfect competition
 #' @export
 #' @examples
-#' cost1()
+#' cost2()
 
 
 

a8ed9513a6ea0906c7178e750c3db60d6f16a110
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2018-09-11 17:18:06 -0300

    Update grid_2.R

--- R/grid_2.R
+++ R/grid_2.R
@@ -3,26 +3,27 @@
 #' This function creates a grid (more especifically, a 2-cell) of coordinates in R^2. Useful for plotting and generating data points with which to apply some functions.
 #' @param a is the grid's lower bound. Defaults to 0.
 #' @param b is the grid's upper bound. Defaults to 100.
+#' @param c is the "by" parameter, the grid's density. Defaults to .5.
 #' @keywords microeconomics
 #' @export
 #' @examples
 #' grid.2()
 
-grid.2 <- function(a=0, b=100) {
+grid.2 <- function(a=0, b=100, c = .5) {
 
 x = vector()
 y = vector()
 
 x = seq(from = a,
           to = b,
-            by = .5)
+            by = c)
 
 x = sort(rep(x,length(x)),
          decreasing = FALSE)
 
 y = seq(from = a,
           to = b,
-            by = .5)
+            by = c)
 
 y = rep(y,
         length(y))

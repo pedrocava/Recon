c41ce147a1a4bc68c549078e844f7e98d6df2a9f
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2018-09-17 15:27:17 -0300

    cournot

--- R/cournot_2.R
+++ R/cournot_2.R
@@ -12,31 +12,31 @@
 cournot_2 = function(q0_1 = 0, c1 = 1, c1_2 = 0,
                      q0_2 = 0, c2 = 1, c2_2 = 0,
                      p_0, a = 1, b = 0) {
-  
-  firm1 = quote(q0_1 + c1 * q1 + c1_2 * q1) ## firm 1's cost curve
-  firm2 = quote(q0_2 + c2 * q2 + c2_2 * q2) ## firm 2's cost curve
+
+  firm1 = quote(q0_1 + c1 * q1 + c1_2 * q1 ** 2) ## firm 1's cost curve
+  firm2 = quote(q0_2 + c2 * q2 + c2_2 * q2 ** 2) ## firm 2's cost curve
   q = quote(q1 + q2) #total supply
-  
+
   demand = quote(p_0 + a*q + b * q^2) # market demand curve
-  
+
   mg_cost1 = D(firm1, "q1") #firm 1's marginal cost/ supply curve
   mg_cost2 = D(firm2, "q2") # firm 2's marginal cost/ supply curve
-    
-  profit1 = (mg_cost1*demand) - firm1 #each firm's profit curve
-  profit2 = (mg_cost2*demand) - firm2
-  
-  mg_profit1 = D(profit1, q1) #marginal profit
-  mg_profit2 = D(profit2, q2)
-  
+
+  profit1 = quote((p_0 + a*q + b * q^2)*(c1+c1_2*(2*q1)) -1*(q0_1 + c1 * q1 + c1_2 * q1 ** 2)) #each firm's profit curve
+  profit2 = quote((p_0 + a*q + b * q^2)*(c2+c2_2*(2*q2)) -1*(q0_2 + c2 * q2 + c2_2 * q2 ** 2))
+
+  mg_profit1 = D(profit1, "q1") #marginal profits
+  mg_profit2 = D(profit2, "q2")
+
   q1_opt = uniroot(mg_profit1, c( - 1000000, 1000000)) # locates optimal production
   q2_opt = uniroot(mg_profit2, c( - 1000000, 1000000))
-  
+
   q1sh = q1_opt / q #calculates each firm's market share
   q2sh = q2_opt / q
-  
+
   p = demand(q) #calcula o preço de mercado
-  
-  
+
+
   results = list("price" = p,
                  "total output" = q,
                  q1_share = q1sh,
--- R/cournot_2_numeric.R
+++ R/cournot_2_numeric.R
@@ -18,10 +18,10 @@
 
 cournot_2_numeric = function(c1_0 = 0, c1_1 = 1, c1_2 = 0,
                      c2_0 = 0, c2_1 = 1, c2_2 = 0,
-                     p_0 = 0, p_1 = 1, p_2 = 0) {
-  
+                     p_0 = 0, p_1 = -1, p_2 = 0) {
+
   require(rootSolve)
-  
+
   # Define the first order condition functions to search for a fixed point
   focs <- function(q) c(
       foc1 = ( (p_1 + 2*p_2*sum(q))*q[1] + (p_0 + p_1*sum(q) + p_2*sum(q)^2) - (c1_1 + 2*c1_2*q[1]) ),
@@ -30,12 +30,12 @@ cournot_2_numeric = function(c1_0 = 0, c1_1 = 1, c1_2 = 0,
 
   # A good initial guess is to set Q = 0
   q_eq <- multiroot(f = focs, start = c(0, 0))
-  
+
   q_1 <- q_eq$root[1]
   q_2 <- q_eq$root[2]
   p <- p_0 + p_1*sum(q_1+q_2) + p_2*sum(q_1+q_2)^2
-  
-  
+
+
   results = list("price" = p,
                  "output_1" = q_1,
                  "output_2" = q_2)

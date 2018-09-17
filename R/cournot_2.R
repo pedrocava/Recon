0432b0926b102506ce7bb810f47654b42fc37f01
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2018-09-17 15:43:45 -0300

    cournot

--- R/cournot_2.R
+++ R/cournot_2.R
@@ -27,12 +27,8 @@ cournot_2 = function(q0_1 = 0, c1 = 1, c1_2 = 0,
   mg_profit1 = function() {D(profit1, "q1")} #marginal profits
   mg_profit2 = function() {D(profit2, "q2")}
 
-  q1_opt = uniroot(mg_profit1,
-                   lower = - 1000000,
-                   upper = 1000000) # locates optimal production
-  q2_opt = uniroot(mg_profit2,
-                   lower =  1000000,
-                   upper = 1000000)
+  q1_opt = uniroot(mg_profit1) # locates optimal production
+  q2_opt = uniroot(mg_profit2)
 
   q = q1_opt + q2_opt
 

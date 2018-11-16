8ae03c8365dc1008d4e95b39769a6667bd08d06b
marcelogelati <marcelogelati@gmail.com>
2018-11-16 13:04:38 -0200

    fixing sgps

--- R/simultaneous_games_pure.R
+++ R/simultaneous_games_pure.R
@@ -5,48 +5,113 @@
 #' @param b is the column player's payoff matrix.
 #' @export
 
-sgps <- function(a, b){ ## Os argumentos são as matrizes nxn de payoff do jogador das linhas e do jogador das colunas, respectivamente
+sgps <- function(a, b){
 
-  brc <- matrix(NA, nrow = nrow(a)*ncol(a), ncol = 2, dimnames = list(NULL, c("row", "col")))
-  brl <- matrix(NA, nrow = nrow(a)*ncol(a), ncol = 2, dimnames = list(NULL, c("row", "col"))) ## Criando os contâiners das melhores respostas
+# Here I'm creating the container for the best responses
 
-  i <- 1 ## Criando um índice para o while
+listcol <- list()  
+listrow <- list()
 
-  while (i <= nrow(a)) { ## Coletando as melhores respostas do jogador da coluna
-    brc[i, ] <- c(i, which(b[i, ] == max(b[i, ])))
-    i <- i + 1
-  }
+i <- 1 
+  
 
-  i <- 1 # Criando um índice para o while
+# I proceed by appending the best responses to the list 
 
-  while (i <= ncol(a)) { ## Coletando as melhores respostas do jogador da coluna
-    brl[i, ] <- c(which(a[, i] == max(a[, i])), i)
-    i <- i + 1
-  }
+while (i <= nrow(a)) {
+  listcol[[i]]<- which(b[i, ] == max(b[i, ])) 
+  i <- i + 1
+}
 
-  matriz_br <<- list(pos_linha = brl[complete.cases(brl), ], pos_coluna = brc[complete.cases(brc), ]) ## Criando a matriz de melhores respostas
+i <- 1
+
+# Here, I'm naming the list element with its column/row, i.e., if the best 
+# response for row 2 is col 3, then my list will have the second element (3) named 2
+
+while (i <= nrow(a)){
+  names(listcol)[i] <- i
+  i <- i + 1
+}
+
+j <- 1
+
+# Here I proceed as above
+
+while (j <= ncol(b)){
+  listrow[[j]]<- which(a[ , j] == max(a[ , j]))
+  j <- j + 1
+}
+
+j <- 1 
+
+while (j <= ncol(b)){
+  names(listrow)[j] <- j
+  j <- j + 1
+}
 
 
-  i <- 1
-  j <- 1 ## Criando índices para o while
+# Now i'll create another container
 
-  matriz <- matrix(NA, nrow = length(a), ncol = 2, dimnames =
-                     list(paste(rep("Equilibirum", length(a)), seq(1:length(a))), c("row", "col"))) ## Criando o contâiner para os equilíbrios
+rowbr <- list()
+colbr <- list()
 
-  while (i <= nrow(matriz_br$pos_linha)){ ## Achando o equilíbrio de Nash, isto é, comparando as melhores respostas
-    while (j <= nrow(matriz_br$pos_coluna)){
-      if (all(matriz_br$pos_linha[i, ] == matriz_br$pos_coluna[j, ]) == TRUE){
-        matriz[i, ] <- matriz_br$pos_linha[i, ]
-      }
-      j <- j + 1
+# Here I'm unlisting the list and giving a proper name to each element (I had to figure
+# how to solve this problem. If I had two best responses (2, 2) for, let's say, column 3, then
+# we would have an unlisted vector named 31 and 32 on the elements (2, 2). I wanted to 
+# have the names just as the column 3 to have the output as (2, 2) and the names (3, 3))
+
+rowunlisted <- setNames(unlist(listrow, use.names=F),rep(names(listrow), lengths(listrow)))
+colunlisted <- setNames(unlist(listcol, use.names=F),rep(names(listcol), lengths(listcol)))
+
+x <- 1
+
+# Here I'm creating pairs of best responses
+
+while (x <= length(rowunlisted)) {
+  rowbr[[x]] <- c(rowunlisted[x], names(rowunlisted[x]))
+  names(rowbr[[x]]) <- NULL
+  x <- x + 1
+}
+
+x <- 1
+
+# Same as above
+
+while (x <= length(colunlisted)) {
+  colbr[[x]] <- c(names(colunlisted[x]), colunlisted[x])
+  names(colbr[[x]]) <- NULL
+  x <- x + 1
+}
+
+# Creating the final container to print the answer
+
+eqnash <- list()
+
+i <- 1
+j <- 1
+
+# Now I compare each one of the pairs of best responses, and 
+# if they are equal, we have a Nash equilibrium (by definition)
+
+while (i <= length(rowbr)){
+  while (j <= length(colbr)){
+    if (all(rowbr[[i]] == colbr[[j]]) == TRUE){
+      eqnash[[i]] <- rowbr[[i]]
     }
-    j <- 1
-    i <- i + 1
+    j <- j + 1
   }
+  j <- 1
+  i <- i + 1
+}
 
-  if (!is.logical(matriz[complete.cases(matriz), ])) { ## Testando se há equilíbrio
-      print(matriz[complete.cases(matriz), ]) ## Printando resultado
-  } else {
-      print("There's no pure strategies equilibrium", quote = F)
-    }
+# Now I'm removing some NULL values that appear in my output
+
+eqnash[sapply(eqnash, is.null)] <- NULL
+
+## And here I'm printint the answer
+
+if (is.null(unlist(eqnash))){
+    print("There's no pure strategies equilibrium", quote = F)
+} else {
+    print(eqnash)
+  }
 }

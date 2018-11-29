4dff303bab36b6e5f211329a6678cddf6ad92503
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2018-11-29 21:02:28 -0200

    .

--- R/cournot_2_numeric.R
+++ R/cournot_2_numeric.R
@@ -23,13 +23,13 @@ cournot_2_numeric = function(c1_0 = 0, c1_1 = 1, c1_2 = 0,
 
   # Define the first order condition functions to search for a fixed point
   focs <- function(q) c(
-      foc1 = ( (p_1 + 2*p_2*sum(q))*q[1] + (p_0 + p_1*sum(q) + p_2*sum(q)^2) - (c1_1 + 2*c1_2*q[1]) ),
-      foc2 = ( (p_1 + 2*p_2*sum(q))*q[2] + (p_0 + p_1*sum(q) + p_2*sum(q)^2) - (c2_1 + 2*c2_2*q[2]) )
+      foc1 <-  ( (p_1 + 2*p_2*sum(q))*q[1] + (p_0 + p_1*sum(q) + p_2*sum(q)^2) - (c1_1 + 2*c1_2*q[1]) ),
+      foc2 <-  ( (p_1 + 2*p_2*sum(q))*q[2] + (p_0 + p_1*sum(q) + p_2*sum(q)^2) - (c2_1 + 2*c2_2*q[2]) )
     )
 
 
   # A good initial guess is to set Q = 0
-  q_eq <- multiroot(f = focs, start = c(0, 0))
+  q_eq <- rootSolve::multiroot(f = focs, start = c(0, 0))
 
   q_1 <- q_eq$root[1]
   q_2 <- q_eq$root[2]
@@ -38,7 +38,7 @@ cournot_2_numeric = function(c1_0 = 0, c1_1 = 1, c1_2 = 0,
   q_1sh <- q_1/q
   q_2sh <- q_2/q
 
-  results = list("price" = p,
+  results <-  list("price" = p,
                  "output_1" = q_1,
                  "output_2" = q_2,
                  "total_output" = q,

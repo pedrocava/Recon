3f22cfe4f38488b2ead1034805b77e2b1fec6f40
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2019-02-10 19:37:02 -0200

    Update simultaneous_games_mixed.R

--- R/simultaneous_games_mixed.R
+++ R/simultaneous_games_mixed.R
@@ -8,10 +8,10 @@
 sgms <- function(a, b) {
   if (isTRUE(all.equal(dim(a), c(2, 2)))) { ## testing if payoff matrices are 2x2
     x_c <- (a[2,2] - a[1, 2])/(a[1, 1] - a[2, 1] - a[1, 2] + a[2, 2])
-    x_l <- (b[2,2] - b[2, 1])/(b[1, 1] - b[2, 1] - b[1, 2] + b[2, 2]) ## Calculando a probabilidade da coluna e da linha
+    x_l <- (b[2,2] - b[2, 1])/(b[1, 1] - b[2, 1] - b[1, 2] + b[2, 2]) ## calculating probabilities
       if (x_c >= 0 & x_c <= 1) {
         print(c("Probability of playling the first row is =", x_l), quote = F)
-        print(c("Probability of playling the first row is =", x_c), quote = F) ## Printando resultados
+        print(c("Probability of playling the first row is =", x_c), quote = F) 
       } else {
         print("There's no mixed strategies equilibrium", quote = F)
       }
